1418c2df791feb0079457a37c421a9ff76ac96a0
Janishsevak <jenishsevak7@gmail.com>
2024-06-20 12:27:14 +0530

    setup and requirement file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.1',
+    author= 'Janishsevak',
+    author_email= 'Jenishsevak7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
